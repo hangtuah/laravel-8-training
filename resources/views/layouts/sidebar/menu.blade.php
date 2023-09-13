a925ad2d96e9614169d0f021214846ad3927af0f
Mohd Fairus Ahmad <mohdfairusahmad@gmail.com>
2023-09-13 03:22:15 +0000

    template for dashboard

--- resources/views/layouts/sidebar/menu.blade.php
+++ resources/views/layouts/sidebar/menu.blade.php
@@ -19,6 +19,21 @@
                     Messages
                     <span class="badge bg-success-soft text-success ms-auto">2 New!</span>
                 </a>
+                <!--BEGIN::MENU FOR VEHICLE-->
+                <!-- Sidenav Menu Heading (Core)-->
+                <div class="sidenav-menu-heading">Module</div>
+                <!-- Sidenav Accordion (Dashboard)-->
+                <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapseDashboards" aria-expanded="false" aria-controls="collapseDashboards">
+                    <div class="nav-link-icon"><i data-feather="activity"></i></div>
+                    Vehicles
+                    <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseDashboards" data-bs-parent="#accordionSidenav">
+                    <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
+                        <a class="nav-link" href="{{ route('vehicle.index') }}">List</a>
+                    </nav>
+                </div>
+                <!--END::MENU FOR VEHICLE-->
                 <!-- Sidenav Menu Heading (Core)-->
                 <div class="sidenav-menu-heading">Core</div>
                 <!-- Sidenav Accordion (Dashboard)-->
